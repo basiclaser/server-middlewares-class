0dc696d074b4e59f780764d767d6bac0da97f0a9
Christopher Shelley <dont@forgetmy.email>
2020-11-26 15:56:40 +0100

    add middlewares (log,session,compression)and new commands

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
-let rawdata = fs.readFileSync("hnData");
+const morgan = require("morgan");
+const compression = require("compression");
+let rawdata = fs.readFileSync("hnData.json");
+var session = require("express-session");
 let hnData = JSON.parse(rawdata);
 
+const { PORT = 4000, APP_SECRET = "stupid default" } = process.env;
+
 const app = express();
-app.use(cors()); // <- middleware
+
+// CROSS-ORIGIN RESOURCE SHARING
+app.use(
+  cors({
+    origin: ["http://localhost:3001"],
+    methods: ["GET", "POST"],
+  })
+);
+
+app.use(
+  session({
+    secret: APP_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+
+// logging middleware
+app.use(morgan("common"));
+
+app.use(compression());
+
+//TODO: error middleware
+app.use((error, req, res, next) => {
+  res.send(error.message);
+});
 
 app.get("/", (req, res) => {
-  res.json(hnData);
+  if (!req.session.userId) {
+    req.session.userId = 1292387213;
+    res.send("<h1>welcome!</h1>");
+  } else {
+    res.send("<h1>welcome back!</h1>");
+  }
+});
+
+app.get("/login", (req, res) => {
+  req.session.favouriteProduct = "xbox";
+  res.send("logged in without you knowing because im a cookie");
 });
 
 app.get("/hello", (req, res) => {
   res.send("Hello to you to!");
 });
 
-app.listen("4000", () =>
-  console.log("server successfully running on port 4000")
+app.listen(PORT, () =>
+  console.log("server successfully running on port " + PORT)
 );
